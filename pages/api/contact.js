073bfefbb862844bf261272a3a9096d7c227b4cf
unknown <majdi.mokhtar@gmail.com>
2022-11-08 00:03:00 +0100

    next config

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -21,7 +21,6 @@ async function handler(req, res) {
       message,
     }
     let client
-    // "mongodb+srv://majdi-next:8BffHQXbs8hDkesj@cluster0.x5apesq.mongodb.net/my-blog?retryWrites=true&w=majority"
     const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster}.x5apesq.mongodb.net/${process.env.mongodb_database_key}?retryWrites=true&w=majority`
     try {
       client = await MongoClient.connect(connectionString)
