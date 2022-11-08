f30bb1b35927456194aec20af7e39b5614928f61
unknown <majdi.mokhtar@gmail.com>
2022-11-08 09:59:29 +0100

    syntax highlighter light

--- components/posts/post-details/post-content.js
+++ components/posts/post-details/post-content.js
@@ -2,8 +2,10 @@ import styles from "./post-content.module.css"
 import PostHeader from "./post-header"
 import ReactMarkdown from "react-markdown"
 import Image from "next/image"
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter"
+import  atomDark  from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark"
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript"
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css"
 
 // const DUMMY_POST = {
 //   slug: "getting-started-with-nextjs",
@@ -12,7 +14,8 @@ import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
 //   content: "# This is first text",
 //   date: "2022-10-02",
 // }
-
+SyntaxHighlighter.registerLanguage("js",js)
+SyntaxHighlighter.registerLanguage("css",css)
 function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`
   const customRenderes = {
